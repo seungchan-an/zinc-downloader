4f6b1ccd0704a3977cf5fbdd7031adee5e398b2a
Seungchan An <ann081993@gmail.com>
2025-11-11 04:51:01 +0900

    update __all__ export to include check_urls

--- src/zincdl/__init__.py
+++ src/zincdl/__init__.py
@@ -1,7 +1,8 @@
 from .core import generate_urls
-from .download import download_tranches
+from .download import check_urls, download_tranches
 
 __all__ = [
     "generate_urls",
+    "check_urls",
     "download_tranches",
 ]
